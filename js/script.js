b2719999622b84c47e6b18c1c5e0e500ffc851aa
Vieyra <vieyraurenabravo@gmail.com>
2020-12-01 11:37:32 -0600

    Se agrega la funcionalidad del formulario

--- js/script.js
+++ js/script.js
@@ -7,36 +7,49 @@ const URL = 'https://js2-contact-form-api.netlify.app/api/contact';
 const form = document.getElementById('new-info-form');
 const infoList = document.getElementById('info-list');
 const img = document.getElementById('img');
+const message = 'Teléfono: No fue proporcionado';
 
 const appendInfoDOM = (datas) => {
   const modalList = document.createElement('div');
   modalList.className = 'modal-content';
+  const h3 = document.createElement('h3');
+  h3.innerHTML = 'Su mensaje ha sido enviado';
+  const list = document.createElement('ul');
+  const nombre = document.createElement('li');
+  nombre.innerHTML = `Nombre: ${datas.name}`;
+  const email = document.createElement('li');
+  email.innerHTML = `Email: ${datas.email}`;
+  const tel = document.createElement('li');
+  if (datas.phone === null) {
+    tel.innerHTML = message;
+  } else {
+    tel.innerHTML = `Teléfono: ${datas.phone}`;
+  }
+  const asunto = document.createElement('li');
+  asunto.innerHTML = `Asunto: ${datas.subject}`;
+  const msj = document.createElement('li');
+  msj.innerHTML = `Mensaje: ${datas.message}`;
+  const parag = document.createElement('p');
+  parag.innerHTML = 'Pronto nos comunicaremos con usted. Gracias por su preferencia.';
+  list.appendChild(nombre);
+  list.appendChild(email);
+  list.appendChild(tel);
+  list.appendChild(asunto);
+  list.appendChild(msj);
+  modalList.appendChild(h3);
+  modalList.appendChild(list);
+  modalList.appendChild(parag);
   infoList.appendChild(modalList);
-  const content = `
-    <span id="equis">X</span>
-    <h3>Su mensaje ha sido enviado</h3>
-    <ul>
-    <li>Nombre: ${datas.name}</li>
-    <li>Email: ${datas.mail}</li>
-    <li>Teléfono: ${datas.phone}</li>
-    <li>Asunto: ${datas.subject}</li>
-    <li>Mensaje: ${datas.message}</li>
-    </ul>
-    <p>Pronto nos comunicaremos con usted. Gracias por su preferencia.</p>
-    <button id="ok" class="btn">OK</button>
-  `;
-  console.log(modalList.innerHTML = content);
 };
 
 const addInf = (nombre, correo, telefono, asunto, mensaje) => {
   const newInf = {
     name: nombre,
-    mail: correo,
+    email: correo,
     phone: telefono,
     subject: asunto,
     message: mensaje,
   };
-  console.log(newInf);
 
   const fetchInfo = {
     method: 'POST',
@@ -47,7 +60,6 @@ const addInf = (nombre, correo, telefono, asunto, mensaje) => {
     .then((response) => response.json())
     .then((data) => {
       appendInfoDOM(data);
-      console.log(data);
     });
 };
 
@@ -69,10 +81,3 @@ form.addEventListener('submit', (event) => {
   form.elements[3].value = '';
   form.elements[4].value = '';
 });
-
-// const closeModal = document.querySelectorAll('modal-content');
-// function close(event) {
-//   event.preventDefault();
-//   infoList.classList.remove('display');
-// }
-// closeModal.addEventListener('click', close);
